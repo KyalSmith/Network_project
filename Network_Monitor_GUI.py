6dc6ab664af0b59d842f9fd117face224325791b
Kyal Smith <kyal.smith@02corm-m1qwfvh5.za.ds.naspers.com>
2017-10-05 10:14:45 +0200

    network_backup

---
+++ Network_Monitor_GUI.py
@@ -0,0 +1,156 @@
+from PyQt4 import QtCore,QtGui
+import sys
+import threading
+import network_monitor
+import pyqtgraph as pg
+import time
+import subprocess
+
+
+
+
+
+class test_form(QtGui.QMainWindow):
+    def __init__(self):
+        super(test_form,self).__init__()
+        self.setup_gui()
+        self.start_time = time.time()
+        check_thread = threading.Thread(target=self.run_check,daemon=True)
+        check_thread.start()
+        self.widg.show()
+
+
+    def setup_gui(self):
+
+        self.setWindowTitle("Network Monitoring App")
+        self.widg = QtGui.QWidget()
+        self.incoming_lbl = QtGui.QLabel("Incoming: ")
+        self.outgoing_lbl = QtGui.QLabel("Outgoing: ")
+        self.incoming_val = QtGui.QLabel("0")
+        self.incoming_val.setStyleSheet("color: Green")
+        self.incoming_metric = QtGui.QLabel("KB/s")
+        self.outgoing_val = QtGui.QLabel("0")
+        self.outgoing_val.setStyleSheet("color: Red")
+        self.outgoing_metric = QtGui.QLabel("KB/s")
+        self.total_incoming_lbl = QtGui.QLabel("Total Received: ")
+        self.total_incoming_val = QtGui.QLabel("0")
+        self.total_incoming_val.setStyleSheet("Color:Green")
+        self.total_incoming_metric = QtGui.QLabel("KB/s")
+        self.total_outgoing_lbl = QtGui.QLabel("Total Send: ")
+        self.total_outgoing_val = QtGui.QLabel("0")
+        self.total_outgoing_val.setStyleSheet("color: Red")
+        self.total_outgoing_metric = QtGui.QLabel("KB/s")
+        self.time_lbl = QtGui.QLabel("Date Time: ")
+        self.time_val = QtGui.QLabel("")
+
+        self.graph = pg.GraphicsWindow()
+        self.incoming_plot = self.graph.addPlot(title='Incoming')
+
+        self.incoming_curve = self.incoming_plot.plot()
+        self.outgoing_plot = self.graph.addPlot(title='Outgoing')
+        self.outgoing_curve = self.outgoing_plot.plot()
+
+        self.layout = QtGui.QHBoxLayout()
+        self.graph_layout = QtGui.QVBoxLayout()
+        self.label_layout = QtGui.QVBoxLayout()
+        self.label_incoming = QtGui.QHBoxLayout()
+        self.label_outgoing = QtGui.QHBoxLayout()
+        self.label_total_incoming = QtGui.QHBoxLayout()
+        self.label_total_outgoing = QtGui.QHBoxLayout()
+        self.label_time = QtGui.QHBoxLayout()
+
+        self.label_layout.addLayout(self.label_incoming)
+        self.label_layout.addLayout(self.label_outgoing)
+        self.label_layout.addLayout(self.label_total_incoming)
+        self.label_layout.addLayout(self.label_total_outgoing)
+        self.label_layout.addLayout(self.label_time)
+        self.label_layout.setAlignment(QtCore.Qt.AlignTop)
+
+        self.layout.addLayout(self.graph_layout)
+        self.layout.addLayout(self.label_layout)
+
+        self.widg.setLayout(self.layout)
+        self.label_incoming.addWidget(self.incoming_lbl)
+        self.label_incoming.addWidget(self.incoming_val)
+        self.label_incoming.addWidget(self.incoming_metric)
+        self.label_outgoing.addWidget(self.outgoing_lbl)
+        self.label_outgoing.addWidget(self.outgoing_val)
+        self.label_outgoing.addWidget(self.outgoing_metric)
+        self.label_total_incoming.addWidget(self.total_incoming_lbl)
+        self.label_total_incoming.addWidget(self.total_incoming_val)
+        self.label_total_incoming.addWidget(self.total_incoming_metric)
+        self.label_total_outgoing.addWidget(self.total_outgoing_lbl)
+        self.label_total_outgoing.addWidget(self.total_outgoing_val)
+        self.label_total_outgoing.addWidget(self.total_outgoing_metric)
+        self.label_time.addWidget(self.time_lbl)
+        self.label_time.addWidget(self.time_val)
+        self.graph_layout.addWidget(self.graph)
+
+    def run_check(self):
+
+        self.bytes_recv = 0
+        self.bytes_sent = 0
+        self.prev_val = []
+        self.incoming_list = []
+        self.outgoing_list = []
+        self.time = []
+
+
+        while True:
+
+            self.current_time = time.time()-self.start_time
+            self.val = network_monitor.collect_data()
+            self.val_lst = self.val.split(":")
+            if not self.prev_val:
+
+                pass
+
+            else:
+                self.time.append(self.current_time)
+                self.bytes_recv = float(self.val_lst[0]) - float(self.prev_val[0])
+                self.incoming_list.append(int(self.bytes_recv))
+                self.bytes_sent = float(self.val_lst[1]) - float(self.prev_val[1])
+                self.outgoing_list.append(int(self.bytes_sent))
+
+                if self.bytes_recv < 0:
+
+                    self.bytes_recv = 0
+
+                if self.bytes_sent < 0:
+
+                    self.bytes_sent = 0
+
+                self.incoming_val.setText(str(round(self.bytes_recv, 2)))
+                self.outgoing_val.setText(str(round(self.bytes_sent, 2)))
+
+            self.prev_val = self.val_lst
+
+            #print("Time: "+str(round(self.current_time,2)) +"\tIncoming: "+str(round(self.bytes_recv,2))+"\tOutgoing: "+str(round(self.bytes_sent,2)))
+
+            self.incoming_curve.setData(self.time,self.incoming_list,pen='g')
+            self.outgoing_curve.setData(self.time,self.outgoing_list,pen='r')
+            self.find_total()
+
+
+    def find_total(self):
+        latest = subprocess.check_output("tail -1 stats.csv",shell=True)
+        result_list = latest.decode('utf-8').split(",")
+        time = result_list[2]
+
+        self.total_incoming_val.setText(str(round(float(result_list[0])/1000,2)))
+        self.total_outgoing_val.setText(str(round(float(result_list[1])/1000,2)))
+        self.time_val.setText(time[:19])
+
+
+
+
+
+
+
+
+
+
+app = QtGui.QApplication(sys.argv)
+window = test_form()
+sys.exit(app.exec_())
+
---
+++ network_monitor.py
@@ -0,0 +1,59 @@
+import psutil
+import os
+from datetime import datetime
+import time
+
+
+
+
+def collect_data():
+
+    tmp_bytes = {"bytes_recv":0,"bytes_sent":0}
+
+
+    tmp_bytes = {}
+    n = 0.25
+    time.sleep(n)
+    os.system("clear")
+    raw_vals = str(psutil.net_io_counters(True)["en3"])
+    raw_vals = raw_vals[7:len(raw_vals) - 1]
+    tmp_lst = raw_vals.split(', ')
+    val_dict = {}
+
+    for val in tmp_lst:
+        tmp = val.split("=")
+        val_dict[tmp[0]] = tmp[1]
+
+    tmp_bytes["bytes_sent"] = int(val_dict["bytes_sent"])/n
+    tmp_bytes["bytes_recv"] = int(val_dict["bytes_recv"])/n
+
+
+
+    if check_file_exists("stats.csv"):
+        bytes_wite = open("stats.csv",'a')
+        bytes_wite.write(str(int(tmp_bytes["bytes_recv"])/1000)+","+str(int(tmp_bytes["bytes_sent"])/1000)+","+str(datetime.now())+"\n")
+
+
+    return_values = str(int(tmp_bytes["bytes_recv"])/1000) + ":" + str(int(tmp_bytes["bytes_sent"])/1000)
+      
+    return return_values
+       
+
+def check_file_exists(file):
+    try:
+        f = open(file,'r')
+    except:
+        f = open(file,'w')
+        f.close()
+        return False
+    else:
+        return True
+        f.close()
+
+
+
+
+
+
+
+
